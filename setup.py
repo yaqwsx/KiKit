903d8234b0833006b066d748e1880e477b2b736c
Jan Mrázek <email@honzamrazek.cz>
2020-04-11 12:49:53 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="KiKit",
-    version="0.2.2",
+    version="0.2.4",
     author="Jan Mrázek",
     author_email="email@honzamrazek.cz",
     description="Automatization for KiCAD boards",
