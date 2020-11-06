c63e53890218e2285abc47a9509e7dc433724327
Marian Buschsieweke <marian.buschsieweke@ovgu.de>
2020-11-06 23:24:14 +0100

    kikit/fab: Add support for PCBWay

--- kikit/fab_ui.py
+++ kikit/fab_ui.py
@@ -20,6 +20,37 @@ def jlcpcb(**kwargs):
     return jlcpcb.exportJlcpcb(**kwargs)
 
 
+@click.command()
+@click.argument("board", type=click.Path(dir_okay=False))
+@click.argument("outputdir", type=click.Path(file_okay=False))
+@click.option("--assembly/--no-assembly", help="Generate files for SMT assembly (schematics is required)")
+@click.option("--schematic", type=click.Path(dir_okay=False), help="Board schematics (required for assembly files)")
+@click.option("--ignore", type=str, default="", help="Comma separated list of designators to exclude from SMT assembly")
+@click.option("--corrections", type=str, default="PCBWAY_CORRECTION",
+    help="Comma separated list of component fields with the correction value. First existing field is used")
+@click.option("--manufacturer", type=str, default="Manufacturer",
+    help="Comma separated list of fields to extract manufacturer name from. First existing field is used.")
+@click.option("--partNumber", type=str, default="PartNumber",
+    help="Comma separated list of fields to extract part number from. First existing field is used.")
+@click.option("--description", type=str, default="Description",
+    help="Comma separated list of fields to extract description from. First existing field is used.")
+@click.option("--notes", type=str, default="Notes",
+    help="Comma separated list of fields to extract notes from. First existing field is used.")
+@click.option("--solderType", type=str, default="Type",
+    help="Comma separated list of fields to extract solder type from. First existing field is used.")
+@click.option("--footprint", type=str, default="FootprintPCBWay",
+    help="Comma separated list of fields to extract the footprint name for the BOM. First existing field is used, otherwise the footprint library name.")
+@click.option("--nBoards", type=int, default=1,
+    help="Number of boards per panel (default 1).")
+@click.option("--missingError/--missingWarn", help="If a non-ignored component misses Manufacturer / PartNumber field, fail")
+def pcbway(**kwargs):
+    """
+    Prepare fabrication files for PCBWAY including their assembly service
+    """
+    from kikit.fab import pcbway
+    return pcbway.exportPcbway(**kwargs)
+
+
 @click.group()
 def fab():
     """
@@ -28,3 +59,4 @@ def fab():
     pass
 
 fab.add_command(jlcpcb)
+fab.add_command(pcbway)
