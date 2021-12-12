65ceb8a2b2f6dafc7bf5d16a4b1d7ee8f25713e7
Jan Mrázek <email@honzamrazek.cz>
2021-12-12 10:47:36 +0100

    Run test on both, kicad-v5 and kicad-v6

--- .github/actions/setup-kicad/setup.sh
+++ .github/actions/setup-kicad/setup.sh
@@ -14,8 +14,9 @@ case $1 in
     sudo apt-get update
     sudo apt-get install --yes --no-install-recommends kicad-nightly
     echo "PYTHONPATH=/usr/lib/kicad-nightly/lib/python3/dist-packages/" >> $GITHUB_ENV
-    for $bin in kicad pcbnew eeschema; do
-        sudo ln -s /usr/bin/$bin-nightly /usr/bin/$bin
+    echo "LD_LIBRARY_PATH=/usr/lib/kicad-nightly/lib/x86_64-linux-gnu:/usr/lib/kicad-nightly/lib/" >> $GITHUB_ENV
+    for bin in kicad pcbnew eeschema; do
+        sudo ln -s /usr/bin/${bin}-nightly /usr/bin/${bin}
     done
     ;;
 
