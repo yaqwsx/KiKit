ed9b3f9f76bac0a27f641defea5fa4e8c169e17e
Jan Mrázek <email@honzamrazek.cz>
2022-07-04 12:57:19 +0200

    Drop v5 support

--- setup.py
+++ setup.py
@@ -7,6 +7,10 @@ import sys
 
 try:
     import pcbnew
+
+    v = [int(x) for x in pcbnew.GetMajorMinorVersion().split(".")]
+    if v[0] == 6 and v[1] == 6.99:
+        print("KiCAD nightly is not supported at the moment. You can try use it, but most functionality will be broken.")
 except ImportError:
     if os.name == "nt":
         message = "No Pcbnew Python module found.\n" + \
@@ -22,6 +26,8 @@ except ImportError:
     sys.stderr.write(
         delimiter + f"** Cannot install KiKit**\n{message}\n" + delimiter)
     raise RuntimeError("Cannot install KiKit, see error message above") from None
+except AttributeError:
+    raise RuntimeError("KiCAD v5 is no longer supported for KiKit. Version v1.0.x is the last one that supports KiCAD 5.")
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
