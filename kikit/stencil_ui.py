0ae49e9ee3fc20ab4a365b6e5f0757f525d1abc6
Wannes Sels <wannes@wsels.be>
2022-09-24 16:31:50 +0200

    PR comments: revert formatting change for click.option help

--- kikit/stencil_ui.py
+++ kikit/stencil_ui.py
@@ -53,14 +53,22 @@ def createPrinted(**kwargs):
 @click.command()
 @click.argument("inputBoard", type=click.Path(dir_okay=False))
 @click.argument("outputDir", type=click.Path(dir_okay=True))
-@click.option("--jigsize", type=(int, int), default=(100, 100), help="Jig frame size in mm: <width> <height>")
-@click.option("--jigthickness", type=float, default=3, help="Jig thickness in mm")
-@click.option("--pcbthickness", type=float, default=1.6, help="PCB thickness in mm")
-@click.option("--registerborder", type=(float, float), default=(3, 1), help="Register borders in mm: <outer> <inner>")
-@click.option("--tolerance", type=float, default=0.05, help="Enlarges the register by the tolerance value")
-@click.option("--ignore", type=str, default="", help="Comma separated list of components references to exclude from the stencil")
-@click.option("--cutout", type=str, default="", help="Comma separated list of components references to cutout from the stencil based on the courtyard")
-@click.option("--type", type=EnumChoice(StencilType, case_sensitive=False), default="solderpaste", help="Stencil for SolderPaste or Adhesive")
+@click.option("--jigsize", type=(int, int), default=(100, 100),
+    help="Jig frame size in mm: <width> <height>")
+@click.option("--jigthickness", type=float, default=3,
+    help="Jig thickness in mm")
+@click.option("--pcbthickness", type=float, default=1.6,
+    help="PCB thickness in mm")
+@click.option("--registerborder", type=(float, float), default=(3, 1),
+    help="Register borders in mm: <outer> <inner>")
+@click.option("--tolerance", type=float, default=0.05,
+    help="Enlarges the register by the tolerance value")
+@click.option("--ignore", type=str, default="",
+    help="Comma separated list of components references to exclude from the stencil")
+@click.option("--cutout", type=str, default="",
+    help="Comma separated list of components references to cutout from the stencil based on the courtyard")
+@click.option("--type", type=EnumChoice(StencilType, case_sensitive=False), default="solderpaste",
+    help="Stencil for SolderPaste or Adhesive")
 def create(**kwargs):
     """
     Create stencil and register elements for manual paste dispensing jig.
