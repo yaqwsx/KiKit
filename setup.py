14bc40631a5ef781ca42f36ee1e011736d137d39
Jan Mrázek <email@honzamrazek.cz>
2023-05-30 17:19:59 +0200

    Encorce Shapely < 2.0

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ setuptools.setup(
     install_requires=[
         "numpy", # Required for MacOS
         "pcbnewTransition >= 0.3.4, <=0.4",
-        "shapely>=1.7",
+        "shapely>=1.7,<2.0",
         "click>=7.1",
         "markdown2>=2.4",
         "pybars3>=0.9",
