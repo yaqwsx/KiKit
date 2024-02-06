10a5b9426c00f0dac955814728c6f8a92df59c1c
Jan Mrázek <email@honzamrazek.cz>
2024-02-06 08:36:24 +0100

    Test nightly in CI

--- .github/actions/setup-kicad/setup.sh
+++ .github/actions/setup-kicad/setup.sh
@@ -43,7 +43,7 @@ case $1 in
     sudo apt-get install --yes --no-install-recommends kicad-nightly
     echo "PYTHONPATH=/usr/lib/kicad-nightly/lib/python3/dist-packages/" >> $GITHUB_ENV
     echo "LD_LIBRARY_PATH=/usr/lib/kicad-nightly/lib/x86_64-linux-gnu:/usr/lib/kicad-nightly/lib/" >> $GITHUB_ENV
-    for bin in kicad pcbnew eeschema; do
+    for bin in kicad pcbnew eeschema kicad-cli; do
         sudo ln -s /usr/bin/${bin}-nightly /usr/bin/${bin}
     done
     ;;
