e0c897b7c1cc1c6d77a669bfac51d7a5a50e616f
Jan Mrázek <email@honzamrazek.cz>
2024-02-24 10:01:43 +0100

    Update CI to use KiCAD 8

--- .github/actions/setup-kicad/setup.sh
+++ .github/actions/setup-kicad/setup.sh
@@ -37,6 +37,32 @@ case $1 in
     echo "PYTHONPATH=/usr/lib/kicad/lib/python3/dist-packages:${PYTHONPATH}" >> $GITHUB_ENV
     echo "LD_LIBRARY_PATH=/usr/lib/kicad/lib/x86_64-linux-gnu/:${LD_LIBRARY_PATH}" >> $GITHUB_ENV
     ;;
+  'v8')
+    sudo add-apt-repository --yes ppa:kicad/kicad-8.0-releases
+    sudo apt-get update
+    sudo apt-get install --yes --no-install-recommends kicad
+    # The Pcbnew module is located in
+    # - /usr/lib/kicad/lib/python3/dist-packages
+    # - instead of /usr/lib/python3/dist-packages/pcbnew.py
+    # Let's add it to PYTHONPATH and also set LD_LIBRARY_PATH
+    echo "PYTHONPATH=/usr/lib/kicad/lib/python3/dist-packages:${PYTHONPATH}" >> $GITHUB_ENV
+    echo "LD_LIBRARY_PATH=/usr/lib/kicad/lib/x86_64-linux-gnu/:${LD_LIBRARY_PATH}" >> $GITHUB_ENV
+    echo "PYTHONPATH=/usr/lib/kicad/lib/python3/dist-packages/:/usr/lib/kicad/local/lib/python3.10/dist-packages:/usr/lib/kicad/local/lib/python3.11/dist-packages:/usr/lib/kicad/local/lib/python3.12/dist-packages" >> $GITHUB_ENV
+    echo "LD_LIBRARY_PATH=/usr/lib/kicad/lib/x86_64-linux-gnu:/usr/lib/kicad/lib/" >> $GITHUB_ENV
+    ;;
+  'v8-testing')
+    sudo add-apt-repository --yes ppa:kicad/kicad-8.0-nightly
+    sudo apt-get update
+    sudo apt-get install --yes --no-install-recommends kicad
+    # The Pcbnew module is located in
+    # - /usr/lib/kicad/lib/python3/dist-packages
+    # - instead of /usr/lib/python3/dist-packages/pcbnew.py
+    # Let's add it to PYTHONPATH and also set LD_LIBRARY_PATH
+    echo "PYTHONPATH=/usr/lib/kicad/lib/python3/dist-packages:${PYTHONPATH}" >> $GITHUB_ENV
+    echo "LD_LIBRARY_PATH=/usr/lib/kicad/lib/x86_64-linux-gnu/:${LD_LIBRARY_PATH}" >> $GITHUB_ENV
+    echo "PYTHONPATH=/usr/lib/kicad/lib/python3/dist-packages/:/usr/lib/kicad/local/lib/python3.10/dist-packages:/usr/lib/kicad/local/lib/python3.11/dist-packages:/usr/lib/kicad/local/lib/python3.12/dist-packages" >> $GITHUB_ENV
+    echo "LD_LIBRARY_PATH=/usr/lib/kicad/lib/x86_64-linux-gnu:/usr/lib/kicad/lib/" >> $GITHUB_ENV
+    ;;
   'nightly')
     sudo add-apt-repository --yes ppa:kicad/kicad-dev-nightly
     sudo apt-get update
@@ -49,7 +75,7 @@ case $1 in
     ;;
 
   *)
-    echo "Invalid version '$1' passed. Only 'v5', 'v6' and 'nightly' supported" >&2
+    echo "Invalid version '$1' passed. Only 'v5', 'v6', 'v7', 'v7-testing', 'v8', 'v8-testing' and 'nightly' supported" >&2
     exit 1
     ;;
 esac
