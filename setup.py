dca72c66d0841b194324163765520aa2fb42a11c
Jan Mrázek <email@honzamrazek.cz>
2026-04-06 21:07:11 +0200

    Remove pcbnew check as new Python installations use isolation
    Python shipped with KiCAD 10 on Windows uses a fresh and isolated python process for the isolation. Hence, we cannot check for the presence of the pcbnew module. The easiest solution is to remove these checks.

--- setup.py
+++ setup.py
@@ -2,37 +2,6 @@
 
 import setuptools
 import versioneer
-import os
-import sys
-
-# Some packages on Linux for v7 change the location of the pcbnew module, let's
-# add the new location to path:
-import os
-if os.name != "nt":
-    sys.path.append("/usr/lib/kicad/lib/python3/dist-packages")
-    try:
-        from ctypes import cdll
-        cdll.LoadLibrary("/usr/lib/kicad/lib/x86_64-linux-gnu/libkicad_3dsg.so.2.0.0")
-    except Exception:
-        pass # Ignore any errors as the library just might not exists here
-
-try:
-    import pcbnew
-except ImportError:
-    if os.name == "nt":
-        message = "No Pcbnew Python module found.\n" + \
-                  "Please make sure that you use KiCAD command prompt, " + \
-                  "not the standard Command Prompt or Power Shell\n" + \
-                  "See https://yaqwsx.github.io/KiKit/latest/installation/windows/."
-    else:
-        message = "No Pcbnew Python module found for the current Python interpreter.\n" + \
-                  "First, make sure that KiCAD is actually installed\n." + \
-                  "Then, make sure that you use the same Python interpreter as KiCAD uses.\n" + \
-                  "Usually a good way is to invoke 'python3 -m pip install kikit'."
-    delimiter = 100 * "=" + "\n" + 100 * "=" + "\n"
-    sys.stderr.write(
-        delimiter + f"** Cannot install KiKit**\n{message}\n" + delimiter)
-    raise RuntimeError("Cannot install KiKit, see error message above") from None
 
 
 with open("README.md", "r") as fh:
